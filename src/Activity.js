3c5fbe7a5b2918ee249e057eacab5a1e6cb35c19
Justin Anthony <justincanthony@gmail.com>
2021-07-30 12:57:10 -0600

    No Cahnge

--- src/Activity.js
+++ src/Activity.js
@@ -1,28 +1,33 @@
 class Activity {
-  constructor(data, userRepository) {
-    this.userId = data.userID;
-    this.date = data.date;
-    this.steps = data.numSteps;
-    this.minutesActive = data.minutesActive;
-    this.flightsOfStairs = data.flightsOfStairs;
-    this.milesWalked = 0;
-    this.reachedStepGoal = null;
-    this.doActivity(userRepository);
+  constructor(activityData) {
+    this.activityData = activityData;
+    // this.userId = data.userID;
+    // this.date = data.date;
+    // this.steps = data.numSteps;
+    // this.minutesActive = data.minutesActive;
+    // this.flightsOfStairs = data.flightsOfStairs;
+    // this.milesWalked = 0;
+    // this.reachedStepGoal = null;
+    // this.doActivity(userRepository);
   }
-  doActivity(userRepo) {
-    var activity = this;
-    userRepo.users.find(function(user) {
-      return user.id === activity.userId;
-    }).updateActivities(this);
-  }
+
+  // doActivity(userRepo) {
+  //   var activity = this;
+  //   userRepo.users.find(function(user) {
+  //     return user.id === activity.userId;
+  //   }).updateActivities(this);
+  // }
+
   calculateMiles(userRepository) {
-    let walkingUser = userRepository.users.find(user => {
+    let walkingUser = userRepository.users.find((user) => {
       return user.id === this.userId;
     });
-    return Math.round(this.steps * walkingUser.strideLength / 5280).toFixed(1);
+    return Math.round((this.steps * walkingUser.strideLength) / 5280).toFixed(
+      1
+    );
   }
   compareStepGoal(userRepository) {
-    let userStepGoal = userRepository.users.find(user => {
+    let userStepGoal = userRepository.users.find((user) => {
       return user.id === this.userId;
     }).dailyStepGoal;
     this.reachedStepGoal = this.steps >= userStepGoal;
--- src/Sleep.js
+++ src/Sleep.js
@@ -1,17 +1,19 @@
-class Sleep { // instance for the user's sleep each day
-  constructor(data, userRepository) {
-    this.userId = data.userID;
-    this.date = data.date;
-    this.hoursSlept = data.hoursSlept;
-    this.sleepQuality = data.sleepQuality;
-    this.sleep(userRepository);
+class Sleep {
+  // instance for the user's sleep each day
+  constructor(sleepData) {
+    this.sleepData = sleepData;
+    // this.userId = data.userID;
+    // this.date = data.date;
+    // this.hoursSlept = data.hoursSlept;
+    // this.sleepQuality = data.sleepQuality;
+    // this.sleep(userRepository);
   }
-  sleep(userRepo) {
-    var sleep = this;
-    userRepo.users.find(function(user) {
-      return user.id === sleep.userId;
-    }).updateSleep(this.date, this.hoursSlept, this.sleepQuality);
-  }
+  // sleep(userRepo) {
+  //   var sleep = this;
+  //   userRepo.users.find(function(user) {
+  //     return user.id === sleep.userId;
+  //   }).updateSleep(this.date, this.hoursSlept, this.sleepQuality);
+  // }
 }
 
 export default Sleep;
