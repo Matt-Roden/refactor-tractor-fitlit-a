f0e52419d6b0a9359fc078037f3ae38ec3ec43f8
Justin Anthony <justincanthony@gmail.com>
2021-07-31 12:36:41 -0600

    Create utils class add function for weekly data

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,27 @@
 class Utility {
-  constructor() 
+  constructor() {}
+
+  getUserOzByWeek(date) {
+    const target = this.userHydration.find(
+      (userEntry) => userEntry.date === date
+    );
+    const index = this.userHydration.indexOf(target);
+    if (index < 7) {
+      let dates = this.userHydration
+        .slice(0, index + 1)
+        .map((userEntry) => userEntry.date);
+      let numOunces = this.userHydration
+        .slice(0, index + 1)
+        .map((userEntry) => userEntry.numOunces);
+      return { date: dates, ounces: numOunces };
+    } else {
+      let dates = this.userHydration
+        .slice(index - 6, index + 1)
+        .map((userEntry) => userEntry.date);
+      let numOunces = this.userHydration
+        .slice(index - 6, index + 1)
+        .map((userEntry) => userEntry.numOunces);
+      return { date: dates, ounces: numOunces };
+    }
+  }
 }
