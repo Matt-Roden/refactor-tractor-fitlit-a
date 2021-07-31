250ae82651ec92adc2e16c50a82f08762e0e32f9
Justin Anthony <justincanthony@gmail.com>
2021-07-31 12:34:46 -0600

    Add function to weekly hydration data

--- src/Hydration.js
+++ src/Hydration.js
@@ -18,6 +18,30 @@ class Hydration {
     return this.userHydration.find((userEntry) => userEntry.date === date)
       .numOunces;
   }
+
+  getUserOzByWeek(date) {
+    const target = this.userHydration.find(
+      (userEntry) => userEntry.date === date
+    );
+    const index = this.userHydration.indexOf(target);
+    if (index < 7) {
+      let dates = this.userHydration
+        .slice(0, index + 1)
+        .map((userEntry) => userEntry.date);
+      let numOunces = this.userHydration
+        .slice(0, index + 1)
+        .map((userEntry) => userEntry.numOunces);
+      return { date: dates, ounces: numOunces };
+    } else {
+      let dates = this.userHydration
+        .slice(index - 6, index + 1)
+        .map((userEntry) => userEntry.date);
+      let numOunces = this.userHydration
+        .slice(index - 6, index + 1)
+        .map((userEntry) => userEntry.numOunces);
+      return { date: dates, ounces: numOunces };
+    }
+  }
 }
 
 export default Hydration;
