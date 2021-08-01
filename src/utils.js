2d16e321b79909ff4367c5fbe80f13eb55c1060c
Justin Anthony <justincanthony@gmail.com>
2021-08-01 12:24:28 -0600

    No change

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,53 @@
-class Utility {
-  constructor() {}
+export class Utility {
+  constructor(data, id) {
+    this.data = data;
+    this.id = id;
+    this.userData = this.data
+      .filter((entry) => entry.id === id)
+      .sort((a, b) => (a.date > b.date ? 1 : -1));
+  }
 
-  getUserOzByWeek(date) {
-    const target = this.userHydration.find(
-      (userEntry) => userEntry.date === date
-    );
-    const index = this.userHydration.indexOf(target);
+  getUserDataByWeek(date, key, property) {
+    const target = this.data.find((userEntry) => userEntry.date === date);
+    const index = this.data.indexOf(target);
     if (index < 7) {
-      let dates = this.userHydration
+      let dates = this.data
         .slice(0, index + 1)
         .map((userEntry) => userEntry.date);
-      let numOunces = this.userHydration
+      let numOunces = this.data
         .slice(0, index + 1)
         .map((userEntry) => userEntry.numOunces);
       return { date: dates, ounces: numOunces };
     } else {
-      let dates = this.userHydration
+      let dates = this.data
         .slice(index - 6, index + 1)
         .map((userEntry) => userEntry.date);
-      let numOunces = this.userHydration
+      let numOunces = this.data
         .slice(index - 6, index + 1)
         .map((userEntry) => userEntry.numOunces);
       return { date: dates, ounces: numOunces };
     }
+  }
+}
+
+getUserSleepByWeek(date, property, key) {
+  const target = this.userSleep.find((userEntry) => userEntry.date === date);
+  const index = this.userSleep.indexOf(target);
+  if (index < 7) {
+    let dates = this.userSleep
+      .slice(0, index + 1)
+      .map((userEntry) => userEntry.date);
+    let sleepValue = this.userSleep
+      .slice(0, index + 1)
+      .map((userEntry) => userEntry[property]);
+    return { date: dates, [key]: sleepValue };
+  } else {
+    let dates = this.userSleep
+      .slice(index - 6, index + 1)
+      .map((userEntry) => userEntry.date);
+    let sleepValue = this.userSleep
+      .slice(index - 6, index + 1)
+      .map((userEntry) => userEntry[property]);
+    return { date: dates, [key]: sleepValue };
   }
 }
