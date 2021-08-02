e36a2c9025199ee68883a5544555c4a4bbbdf014
Justin Anthony <justincanthony@gmail.com>
2021-08-02 10:51:20 -0600

    Celan up scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -13,33 +13,7 @@ let user, userRepository, hydration, sleep, activity;
 
 // function to create random number for user
 const getRandomID = () => Math.floor(Math.random() * 50) + 1;
-
 let randomID = getRandomID();
-
-// getData('users')
-//   .then((data) => {
-//     let userRepository = new UserRepository(data.userData);
-//     let user = new User(userRepository.getUser(randomID));
-//   })
-//   .then(displayUserData);
-
-// getData('hydration')
-//   .then((data) => {
-//     let hydration = new Hydration(data.hydrationData);
-//   })
-//   .then(displayHydrationData);
-
-// getData('sleep')
-//   .then((data) => {
-//     let sleep = new Sleep(data.sleepData);
-//   })
-//   .then(displaySleepData);
-
-// getData('activity')
-//   .then((data) => {
-//     let activity = new Activity(data.activityData);
-//   })
-//   .then(displayActivityData);
 
 getAllData()
   .then((data) => {
